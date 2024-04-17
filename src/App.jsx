5f39d98fc9275d8ecb2f56ad66b20b82e993967f
Vishwas.R <vishwas-r@users.noreply.github.com>
2024-04-17 15:05:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,37 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function App() {
 	const options = {
-		data: [
-			{
-				dataPoints: [
-					{ y: 100 },
-					{ y: 100 },
-					{ y: 100 },
-					{ y: 100 },
-					{ y: 100 },
-				]
-			}
-		]
-
+		animationEnabled: true,
+		exportEnabled: true,
+		theme: "light2", //"light1", "dark1", "dark2"
+		title:{
+			text: "React Column Chart - Vite"
+		},
+		axisY: {
+			includeZero: true
+		},
+		data: [{
+			type: "column", //change type to bar, line, area, pie, etc
+			//indexLabel: "{y}", //Shows y value on all Data Points
+			indexLabelFontColor: "#5A5757",
+			indexLabelPlacement: "outside",
+			dataPoints: [
+				{ x: 10, y: 71 },
+				{ x: 20, y: 55 },
+				{ x: 30, y: 50 },
+				{ x: 40, y: 65 },
+				{ x: 50, y: 71 },
+				{ x: 60, y: 68 },
+				{ x: 70, y: 38 },
+				{ x: 80, y: 92, indexLabel: "Highest" },
+				{ x: 90, y: 54 },
+				{ x: 100, y: 60 },
+				{ x: 110, y: 21 },
+				{ x: 120, y: 49 },
+				{ x: 130, y: 36 }
+			]
+		}]
 	};
-
 	return (
 		<div>
 			<CanvasJSChart options={options} />
